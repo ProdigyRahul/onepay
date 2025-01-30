b44ea86e06070daec8b12c083b0f579b3a54261e
Rahul Mistry <rahulmistry.sde@gmail.com>
2025-01-30 23:41:21 +0530

    fix: add caching and async in nanoid

--- backend/dist/utils/transactionUtils.js
+++ backend/dist/utils/transactionUtils.js
@@ -1,14 +1,58 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateAccountNumber = exports.generateTransactionId = void 0;
-const nanoid_1 = require("nanoid");
-const generateAlphanumeric = (0, nanoid_1.customAlphabet)('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 12);
-const generateNumeric = (0, nanoid_1.customAlphabet)('0123456789', 16);
-const generateTransactionId = () => {
+let generateAlphanumeric;
+let generateNumeric;
+const initializeGenerators = async () => {
+    const { customAlphabet } = await Promise.resolve().then(() => __importStar(require('nanoid')));
+    generateAlphanumeric = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 12);
+    generateNumeric = customAlphabet('0123456789', 16);
+};
+initializeGenerators().catch(console.error);
+const generateTransactionId = async () => {
+    if (!generateAlphanumeric) {
+        await initializeGenerators();
+    }
     return generateAlphanumeric();
 };
 exports.generateTransactionId = generateTransactionId;
-const generateAccountNumber = () => {
+const generateAccountNumber = async () => {
+    if (!generateNumeric) {
+        await initializeGenerators();
+    }
     return generateNumeric();
 };
 exports.generateAccountNumber = generateAccountNumber;
