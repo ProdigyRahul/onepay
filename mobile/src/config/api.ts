769ae963532593522be0db37277d1efc259d9072
Rahul Mistry <rahulmistry.sde@gmail.com>
2025-01-31 09:14:43 +0530

    fix: change uri to production url link

--- mobile/src/config/api.ts
+++ mobile/src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
   // Change this to your API URL
-  // BASE_URL: 'https://onepay-brown.vercel.app/api/',
-  BASE_URL: 'http://192.168.6.202:5000/api/',
+  BASE_URL: 'https://onepay-brown.vercel.app/api/',
+  // BASE_URL: 'http://192.168.6.202:5000/api/',
 } as const;
